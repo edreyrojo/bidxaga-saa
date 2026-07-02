da4a1cd513f8eb171c46165bf668928dedab51b9
edreyrojo <zamna.ed@gmail.com>
2026-07-02 17:37:32 -0600

    se añadieron mas nombres

--- src/data/animales.js
+++ src/data/animales.js
@@ -1,10 +1,56 @@
 export const listaAnimales = [
+    // --- INSECTOS Y BICHITOS ---
     { id: 1, diidxaza: 'Bacuzagií', spanish: 'luciérnaga', image: '/images/bacuzagii.png' }, //
-    { id: 2, diidxaza: 'Biguidi', spanish: 'mariposa', image: '/images/biguidi.png' },     //
-    { id: 3, diidxaza: 'Bidxí', spanish: 'araña', image: '/images/bidxi.png' },           //
-    { id: 4, diidxaza: 'Bi’cu’', spanish: 'perro', image: '/images/bicu.png' },           //
-    { id: 5, diidxaza: 'Bidxiña', spanish: 'venado', image: '/images/bidxina.png' },       //
-    { id: 6, diidxaza: 'Beedxe’', spanish: 'jaguar', image: '/images/beedxe.png' },       //
-    { id: 7, diidxaza: 'Mixhtu’', spanish: 'gato', image: '/images/mixhtu.png' },         //
-    { id: 8, diidxaza: 'Bihui’', spanish: 'cerdo', image: '/images/bihui.png' }           //
+    { id: 2, diidxaza: 'Birí', spanish: 'hormiga', image: '/images/biri.png' }, //
+    { id: 3, diidxaza: 'Biichi’', spanish: 'piojo', image: '/images/biichi.png' }, //
+    { id: 4, diidxaza: 'Bi’chidó', spanish: 'pulga', image: '/images/bichido.png' }, //
+    { id: 5, diidxaza: 'Bichindii', spanish: 'grillo real', image: '/images/bichindii.png' }, //
+    { id: 6, diidxaza: 'Beredxinga', spanish: 'grillo grande', image: '/images/beredxinga.png' }, //
+    { id: 7, diidxaza: 'Biguidi', spanish: 'mariposa', image: '/images/biguidi.png' }, //
+    { id: 8, diidxaza: 'Bizugá’', spanish: 'oruga', image: '/images/bizuga.png' }, //
+    { id: 9, diidxaza: 'Daaya’', spanish: 'mantis', image: '/images/daaya.png' }, //
+    { id: 10, diidxaza: 'Guxha’ru’', spanish: 'chapulín', image: '/images/guxharu.png' }, //
+    { id: 11, diidxaza: 'Xhumpirinisa', spanish: 'libélula', image: '/images/xhumpirinisa.png' }, //
+    { id: 12, diidxaza: 'Bidxí', spanish: 'araña', image: '/images/bidxi.png' }, //
+    { id: 13, diidxaza: 'Bizu', spanish: 'avispa', image: '/images/bizu.png' }, //
+    { id: 14, diidxaza: 'Meuxubi', spanish: 'alacrán', image: '/images/meuxubi.png' }, //
+    { id: 15, diidxaza: 'Bi’chu’', spanish: 'caracol', image: '/images/bichu.png' }, //
+    { id: 16, diidxaza: 'Bito’pe’', spanish: 'cangrejo', image: '/images/bitope.png' }, //
+    { id: 17, diidxaza: 'Bendaboa’', spanish: 'camarón', image: '/images/bendaboa.png' }, //
+
+    // --- MAMÍFEROS SILVESTRES ---
+    { id: 18, diidxaza: 'Beza', spanish: 'zorro', image: '/images/beza.png' }, //
+    { id: 19, diidxaza: 'Biaana', spanish: 'liebre', image: '/images/biaana.png' }, //
+    { id: 20, diidxaza: 'Lempa', spanish: 'conejo', image: '/images/lempa.png' }, //
+    { id: 21, diidxaza: 'Bedxechiiñu’', spanish: 'hormiguero', image: '/images/bedxechiiñu.png' }, //
+    { id: 22, diidxaza: 'Biizi', spanish: 'tlacuache', image: '/images/biizi.png' }, //
+    { id: 23, diidxaza: 'Bi’huigui’xhi’', spanish: 'jabalí', image: '/images/bihuiguixhi.png' }, //
+    { id: 24, diidxaza: 'Bidxiña', spanish: 'venado', image: '/images/bidxina.png' }, //
+    { id: 25, diidxaza: 'Biziña', spanish: 'ratón', image: '/images/bizina.png' }, //
+    { id: 26, diidxaza: 'Huaga', spanish: 'rata', image: '/images/huaga.png' }, //
+    { id: 27, diidxaza: 'Gueu’', spanish: 'coyote', image: '/images/gueu.png' }, //
+    { id: 28, diidxaza: 'Migo', spanish: 'chango', image: '/images/migo.png' }, //
+
+    // --- FELINOS ---
+    { id: 29, diidxaza: 'Beedxe’', spanish: 'jaguar', image: '/images/beedxe.png' }, //
+    { id: 30, diidxaza: 'Bedxelana', spanish: 'pantera', image: '/images/bedxelana.png' }, //
+    { id: 31, diidxaza: 'Bedxegui’xhi', spanish: 'puma', image: '/images/bedxeguixhi.png' }, //
+
+    // --- ANIMALES DOMÉSTICOS Y DE GRANJA ---
+    { id: 32, diidxaza: 'Bi’cu’', spanish: 'perro', image: '/images/bicu.png' }, //
+    { id: 33, diidxaza: 'Mixhtu’', spanish: 'gato', image: '/images/mixhtu.png' }, //
+    { id: 34, diidxaza: 'Mani’', spanish: 'caballo', image: '/images/mani.png' }, //
+    { id: 35, diidxaza: 'Ndxuco', spanish: 'burro', image: '/images/ndxuco.png' }, //
+    { id: 36, diidxaza: 'Dendxu’', spanish: 'borrego', image: '/images/dendxu.png' }, //
+    { id: 37, diidxaza: 'Yuze', spanish: 'res', image: '/images/yuze.png' }, //
+    { id: 38, diidxaza: 'Bihui’', spanish: 'cerdo', image: '/images/bihui.png' }, //
+
+    // --- AVES Y VOLADORES ---
+    { id: 39, diidxaza: 'Biguidiribeela', spanish: 'murciélago', image: '/images/biguidiribeela.png' }, //
+    { id: 40, diidxaza: 'Bere', spanish: 'gallina', image: '/images/bere.png' }, //
+    { id: 41, diidxaza: 'Bereguidxa', spanish: 'correcaminos', image: '/images/bereguidxa.png' }, //
+    { id: 42, diidxaza: 'Da’ma’', spanish: 'tecolote', image: '/images/dama.png' }, //
+    { id: 43, diidxaza: 'Nguengue', spanish: 'loro', image: '/images/nguengue.png' }, //
+    { id: 44, diidxaza: 'Ndxuru’', spanish: 'perico', image: '/images/ndxuru.png' }, //
+    { id: 45, diidxaza: 'Cha’cha’', spanish: 'carpintero', image: '/images/chacha.png' } //
 ];
